6e61acf4cc21669fedf8e6e98ed454ebd5806edd
Felipe Valencia <felipe.valencia@talosdigital.com>
2017-05-01 11:25:11 -0500

    Solved Example 008

--- 008-testing-directives/src/app/components/card/card.directive.spec.js
+++ 008-testing-directives/src/app/components/card/card.directive.spec.js
@@ -2,12 +2,87 @@
   'use strict';
 
   describe('directive card', function() {
-   
+    var $compile,
+    $rootScope,
+    $timeout,
+    suite = {};
+
     beforeEach(module('angularApp'));
 
-    beforeEach(inject(function ($injector) {
-	  /* ... */
+    beforeEach(inject(function ($injector, $templateCache) {
+	  $compile = $injector.get('$compile');
+	  $rootScope = $injector.get('$rootScope');
+	  $timeout = $injector.get('$timeout');
+	  var template = $templateCache.get('app/components/card/card.html');
+	  $templateCache.put('app/components/card/card.html', template);
+
+	  $rootScope.card = {
+	  	title : '',
+		description : '',
+		background : '',
+		textColor : '',
+		icon : '',
+		favorite : function(){
+			return true;
+		},
+		reservedBy : '© Talos Business Card'
+	  };
+
+	  suite.template = 
+	    	'<card title="card.title"'+ 
+		    	'description="card.description"'+
+	            'background="card.background"'+
+	            'text-color="card.textColor"'+
+	            'reserved-by="{{card.reservedBy}}"'+
+	            'cb-favorite="card.favorite(title)"'+
+	            'icon="{{card.icon}}">'+
+      		'</card>';
+
 	}));
+
+	afterEach(function () {
+	    suite.element.remove();
+	    suite = {};
+	});
+
+	afterAll(function () {
+	    suite = null;
+	});
+
+	it('when render without parameters, it has to show only the reservedBy', function () {
+	    suite.element = $compile(angular.element(suite.template))($rootScope);
+	    $rootScope.$digest();
+	    expect(suite.element.find('.reservedBy').text()).toEqual($rootScope.card.reservedBy);
+	});
+
+	it('when render, no errors has to show', function () {
+		$rootScope.card = {
+		  	title : 'Test Title',
+			description : 'Test Description',
+			background : 'rgb(0, 0, 0)',
+			textColor : 'rgb(255, 255, 55)',
+			icon : 'home',
+			favorite : function(title){
+				return title;
+			},
+			reservedBy : '© Talos Business Card'
+		};
+	    suite.element = $compile(angular.element(suite.template))($rootScope);
+	    $rootScope.$digest();
+
+	    spyOn($rootScope.card, 'favorite');
+
+	    expect(suite.element.find('.title').text()).toEqual($rootScope.card.title);
+	    expect(suite.element.find('.description').text()).toEqual($rootScope.card.description);
+	    expect(suite.element.find('.icon').text()).toEqual($rootScope.card.icon);
+	    //Wait until the style is set
+	    $timeout(function(){
+	        expect(suite.element.find('.card').css('background-color')).toEqual($rootScope.card.background);
+	        expect(suite.element.find('.card').css('color')).toEqual($rootScope.card.textColor);
+	    },100);
+	    suite.element.find('.favorite').click();	
+	    expect($rootScope.card.favorite).toHaveBeenCalledWith($rootScope.card.title);
+	});
 
   });
 })();
--- 008-testing-directives/src/app/states/home/home.controller.spec.js
+++ 008-testing-directives/src/app/states/home/home.controller.spec.js
@@ -7,11 +7,57 @@
     beforeEach(module('angularApp'));
 
     beforeEach(inject(function($injector) {
-      suite.$controller = $injector.get('$controller');
+        suite.$controller = $injector.get('$controller');
+      suite.$rootScope = $injector.get('$rootScope');
+      suite.RESERVED_BY = $injector.get('RESERVED_BY');
+      suite.scope = suite.$rootScope.$new();
+
+      suite.vm = suite.$controller('HomeController', {
+        RESERVED_BY: suite.RESERVED_BY,
+        $scope: suite.scope
+      });
+
     }));
+
+    afterEach(function () {
+      suite = {};
+    });
+
+    afterAll(function () {
+        suite = null;
+    });
 
     it('should be registered', function() {
       expect(suite.vm).not.toEqual(null);
+    });
+
+    describe('setFavorite function', function() {
+      it('should exist', function() {
+        expect(suite.vm.card.setFavorite).toBeDefined();
+      });
+
+      it('should add a new favorite', function(){
+        spyOn(suite.vm.card, 'setFavorite').and.callThrough();
+        suite.vm.card.setFavorite('TestTitle');
+        expect(suite.vm.card.setFavorite).toHaveBeenCalledWith('TestTitle');
+        expect(suite.vm.favoriteList).toEqual(['TestTitle']);
+      });
+
+    });
+
+    describe('setIcon function', function() {
+      it('should exist', function() {
+        expect(suite.vm.setIcon).toBeDefined();
+      });
+
+      it('should set new Icon', function(){
+        spyOn(suite.vm, 'setIcon').and.callThrough();
+        suite.vm.setIcon('home');
+        suite.$rootScope.$digest();
+        expect(suite.vm.setIcon).toHaveBeenCalledWith('home');
+        expect(suite.vm.card.icon).toEqual('home');
+      });
+
     });
 
   });
