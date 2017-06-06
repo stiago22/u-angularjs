10efa00cb6f8fbd12d586d1a5f3c62fc7105edf9
Santiago Florez <santiagoflorez@LeidyJs-iMac.local>
2017-06-06 16:22:21 -0500

    Weather service included

--- 003-servers-and-maps/src/app/app.route.js
+++ 003-servers-and-maps/src/app/app.route.js
@@ -10,7 +10,7 @@
     $stateProvider
       .state('home', {
         url: '/',
-        templateUrl: 'app/states/home/home.html',
+        templateUrl: 'app/states/weather/weather.html',
         controller: 'WeatherController',
         controllerAs: 'weatherCtrl'
       });
